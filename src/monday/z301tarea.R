9888d3923898fd48d762b89f5c6e7be7c572fccd
MarieDelga <marielavaninadelgado@gmail.com>
2022-08-29 17:34:15 -0300

    clase 29-8

---
+++ src/monday/z301tarea.R
@@ -0,0 +1,116 @@
+# Limpiamos el entorno
+rm(list = ls())
+gc(verbose = FALSE)
+
+# Librerías necesarias
+require("data.table")
+require("rpart")
+require("ROCR")
+require("ggplot2")
+require("lubridate")
+require("lhs")
+require("DiceKriging")
+require("mlrMBO")
+
+# Poner la carpeta de la materia de SU computadora local
+setwd("C:\\Users\\Marie\\Documents\\MasterUBA\\DMEyF")
+# Poner sus semillas
+semillas <- c(432557, 892597, 998197, 214733, 502321)
+
+# Cargamos el dataset
+dataset <- fread("./datasets/competencia1_2022.csv")
+
+# Nos quedamos solo con el 202101
+dataset <- dataset[foto_mes == 202101]
+
+# Creamos una clase binaria
+dataset[, clase_binaria := ifelse(
+  clase_ternaria == "BAJA+2",
+  "evento",
+  "noevento"
+)]
+# Borramos el target viejo
+dataset[, clase_ternaria := NULL]
+
+# Seteamos nuestra primera semilla
+set.seed(semillas[1])
+
+# funcion ganancia
+ganancia <- function(probabilidades, clase) {
+  return(sum(
+    (probabilidades >= 0.025) * ifelse(clase == "evento", 78000, -2000))
+  )
+}
+
+
+
+# Armamos una función para modelar con el fin de simplificar el código futuro
+modelo_rpart <- function(train, test, cp =  0, ms = 20, mb = 1, md = 10) {
+  modelo <- rpart(clase_binaria ~ ., data = train,
+                  xval = 0,
+                  cp = cp,
+                  minsplit = ms,
+                  minbucket = mb,
+                  maxdepth = md)
+  
+  test_prediccion <- predict(modelo, test, type = "prob")
+  ganancia(test_prediccion[, "evento"], test$clase_binaria) / 0.3
+  
+}
+
+
+# Una función auxiliar para los experimentos
+experimento_rpart <- function(ds, semillas, cp = 0, ms = 20, mb = 1, md = 10) {
+  gan <- c()
+  for (s in semillas) {
+    set.seed(s)
+    in_training <- caret::createDataPartition(ds$clase_binaria, p = 0.70,
+                                              list = FALSE)
+    train  <-  ds[in_training, ]
+    test   <-  ds[-in_training, ]
+    #train_sample <- tomar_muestra(train)
+    r <- modelo_rpart(train, test, 
+                      cp = cp, ms = ms, mb = mb, md = md)
+    gan <- c(gan, r)
+  }
+  mean(gan)
+}
+
+
+
+obj_fun_md_ms <- function(x) {
+  experimento_rpart(dataset, semillas
+                    , md = x$maxdepth
+                    , ms = x$minsplit
+                    , mb = floor(x$minbucket*x$minsplit))
+}
+
+obj_fun <- makeSingleObjectiveFunction(
+  minimize = FALSE,
+  fn = obj_fun_md_ms,
+  par.set = makeParamSet(
+    makeIntegerParam("maxdepth",  lower = 4L, upper = 30L),
+    makeIntegerParam("minsplit",  lower = 1L, upper = 200L),
+    makeNumericParam("minbucket",  lower = 0L, upper = 2L)
+    # makeNumericParam <- para parámetros continuos
+  ),
+  # noisy = TRUE,
+  has.simple.signature = FALSE
+)
+
+ctrl <- makeMBOControl()
+ctrl <- setMBOControlTermination(ctrl, iters = 50L) #modificar mas mejor
+ctrl <- setMBOControlInfill(
+  ctrl,
+  crit = makeMBOInfillCritEI(),
+  opt = "focussearch",
+  # sacar parámetro opt.focussearch.points en próximas ejecuciones
+  opt.focussearch.points = 20
+)
+
+lrn <- makeMBOLearner(ctrl, obj_fun)
+
+surr_km <- makeLearner("regr.km", predict.type = "se", covtype = "matern3_2")
+
+run_md_ms <- mbo(obj_fun, learner = surr_km, control = ctrl, )
+print(run_md_ms)
