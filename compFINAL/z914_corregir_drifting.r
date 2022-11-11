a23f97c188802535949a96e2d4401064fd52a498
MarieDelga <marielavaninadelgado@gmail.com>
2022-11-11 16:53:14 +0000

    primer intento FINAL

---
+++ compFINAL/z914_corregir_drifting.r
@@ -0,0 +1,289 @@
+#require vm con
+#   8 vCPU
+#  64 GB  memoria RAM
+# 256 GB  espacio en disco
+
+
+#limpio la memoria
+rm( list=ls() )  #remove all objects
+gc()             #garbage collection
+
+require("data.table")
+
+
+
+#Parametros del script
+PARAM  <- list()
+PARAM$experimento  <- "DRFINAL1" #DR9141"
+
+PARAM$exp_input  <- "CAFINAL1" #"CA9060"
+
+#valores posibles  "ninguno" "rank_simple" , "rank_cero_fijo" , "deflacion"
+PARAM$metodo  <- "ninguno"
+# FIN Parametros del script
+
+
+#------------------------------------------------------------------------------
+#Esta es la parte que los alumnos deben desplegar todo su ingenio
+#Agregar aqui sus PROPIAS VARIABLES manuales
+
+AgregarVariables  <- function( dataset )
+{
+  gc()
+  #INICIO de la seccion donde se deben hacer cambios con variables nuevas
+
+  #creo un ctr_quarter que tenga en cuenta cuando los clientes hace 3 menos meses que estan
+  dataset[  , ctrx_quarter_normalizado := ctrx_quarter ]
+  dataset[ cliente_antiguedad==1 , ctrx_quarter_normalizado := ctrx_quarter * 5 ]
+  dataset[ cliente_antiguedad==2 , ctrx_quarter_normalizado := ctrx_quarter * 2 ]
+  dataset[ cliente_antiguedad==3 , ctrx_quarter_normalizado := ctrx_quarter * 1.2 ]
+
+  #variable extraida de una tesis de maestria de Irlanda
+  dataset[  , mpayroll_sobre_edad  := mpayroll / cliente_edad ]
+
+  #se crean los nuevos campos para MasterCard  y Visa, teniendo en cuenta los NA's
+  #varias formas de combinar Visa_status y Master_status
+  dataset[ , vm_status01       := pmax( Master_status,  Visa_status, na.rm = TRUE) ]
+  dataset[ , vm_status02       := Master_status +  Visa_status ]
+  dataset[ , vm_status03       := pmax( ifelse( is.na(Master_status), 10, Master_status) , ifelse( is.na(Visa_status), 10, Visa_status) ) ]
+  dataset[ , vm_status04       := ifelse( is.na(Master_status), 10, Master_status)  +  ifelse( is.na(Visa_status), 10, Visa_status)  ]
+  dataset[ , vm_status05       := ifelse( is.na(Master_status), 10, Master_status)  +  100*ifelse( is.na(Visa_status), 10, Visa_status)  ]
+
+  dataset[ , vm_status06       := ifelse( is.na(Visa_status), 
+                                          ifelse( is.na(Master_status), 10, Master_status), 
+                                          Visa_status)  ]
+
+  dataset[ , mv_status07       := ifelse( is.na(Master_status), 
+                                          ifelse( is.na(Visa_status), 10, Visa_status), 
+                                          Master_status)  ]
+
+
+  #combino MasterCard y Visa
+  dataset[ , vm_mfinanciacion_limite := rowSums( cbind( Master_mfinanciacion_limite,  Visa_mfinanciacion_limite) , na.rm=TRUE ) ]
+
+  dataset[ , vm_Fvencimiento         := pmin( Master_Fvencimiento, Visa_Fvencimiento, na.rm = TRUE) ]
+  dataset[ , vm_Finiciomora          := pmin( Master_Finiciomora, Visa_Finiciomora, na.rm = TRUE) ]
+  dataset[ , vm_msaldototal          := rowSums( cbind( Master_msaldototal,  Visa_msaldototal) , na.rm=TRUE ) ]
+  dataset[ , vm_msaldopesos          := rowSums( cbind( Master_msaldopesos,  Visa_msaldopesos) , na.rm=TRUE ) ]
+  dataset[ , vm_msaldodolares        := rowSums( cbind( Master_msaldodolares,  Visa_msaldodolares) , na.rm=TRUE ) ]
+  dataset[ , vm_mconsumospesos       := rowSums( cbind( Master_mconsumospesos,  Visa_mconsumospesos) , na.rm=TRUE ) ]
+  dataset[ , vm_mconsumosdolares     := rowSums( cbind( Master_mconsumosdolares,  Visa_mconsumosdolares) , na.rm=TRUE ) ]
+  dataset[ , vm_mlimitecompra        := rowSums( cbind( Master_mlimitecompra,  Visa_mlimitecompra) , na.rm=TRUE ) ]
+  dataset[ , vm_madelantopesos       := rowSums( cbind( Master_madelantopesos,  Visa_madelantopesos) , na.rm=TRUE ) ]
+  dataset[ , vm_madelantodolares     := rowSums( cbind( Master_madelantodolares,  Visa_madelantodolares) , na.rm=TRUE ) ]
+  dataset[ , vm_fultimo_cierre       := pmax( Master_fultimo_cierre, Visa_fultimo_cierre, na.rm = TRUE) ]
+  dataset[ , vm_mpagado              := rowSums( cbind( Master_mpagado,  Visa_mpagado) , na.rm=TRUE ) ]
+  dataset[ , vm_mpagospesos          := rowSums( cbind( Master_mpagospesos,  Visa_mpagospesos) , na.rm=TRUE ) ]
+  dataset[ , vm_mpagosdolares        := rowSums( cbind( Master_mpagosdolares,  Visa_mpagosdolares) , na.rm=TRUE ) ]
+  dataset[ , vm_fechaalta            := pmax( Master_fechaalta, Visa_fechaalta, na.rm = TRUE) ]
+  dataset[ , vm_mconsumototal        := rowSums( cbind( Master_mconsumototal,  Visa_mconsumototal) , na.rm=TRUE ) ]
+  dataset[ , vm_cconsumos            := rowSums( cbind( Master_cconsumos,  Visa_cconsumos) , na.rm=TRUE ) ]
+  dataset[ , vm_cadelantosefectivo   := rowSums( cbind( Master_cadelantosefectivo,  Visa_cadelantosefectivo) , na.rm=TRUE ) ]
+  dataset[ , vm_mpagominimo          := rowSums( cbind( Master_mpagominimo,  Visa_mpagominimo) , na.rm=TRUE ) ]
+
+  #a partir de aqui juego con la suma de Mastercard y Visa
+  dataset[ , vmr_Master_mlimitecompra:= Master_mlimitecompra / vm_mlimitecompra ]
+  dataset[ , vmr_Visa_mlimitecompra  := Visa_mlimitecompra / vm_mlimitecompra ]
+  dataset[ , vmr_msaldototal         := vm_msaldototal / vm_mlimitecompra ]
+  dataset[ , vmr_msaldopesos         := vm_msaldopesos / vm_mlimitecompra ]
+  dataset[ , vmr_msaldopesos2        := vm_msaldopesos / vm_msaldototal ]
+  dataset[ , vmr_msaldodolares       := vm_msaldodolares / vm_mlimitecompra ]
+  dataset[ , vmr_msaldodolares2      := vm_msaldodolares / vm_msaldototal ]
+  dataset[ , vmr_mconsumospesos      := vm_mconsumospesos / vm_mlimitecompra ]
+  dataset[ , vmr_mconsumosdolares    := vm_mconsumosdolares / vm_mlimitecompra ]
+  dataset[ , vmr_madelantopesos      := vm_madelantopesos / vm_mlimitecompra ]
+  dataset[ , vmr_madelantodolares    := vm_madelantodolares / vm_mlimitecompra ]
+  dataset[ , vmr_mpagado             := vm_mpagado / vm_mlimitecompra ]
+  dataset[ , vmr_mpagospesos         := vm_mpagospesos / vm_mlimitecompra ]
+  dataset[ , vmr_mpagosdolares       := vm_mpagosdolares / vm_mlimitecompra ]
+  dataset[ , vmr_mconsumototal       := vm_mconsumototal  / vm_mlimitecompra ]
+  dataset[ , vmr_mpagominimo         := vm_mpagominimo  / vm_mlimitecompra ]
+
+  #Aqui debe usted agregar sus propias nuevas variables
+  
+  #agregao mis variables
+  
+  #agregamos variables (solo relaciones por linea)
+  
+  dataset$mrentabilidad_annual_pondmes = dataset$mrentabilidad_annual / ifelse(dataset$cliente_antiguedad>12, 12,dataset$cliente_antiguedad)
+  dataset$mrentabilidad_sobre_annual_pondmes = dataset$mrentabilidad / ifelse(dataset$mrentabilidad_annual_pondmes>0, dataset$mrentabilidad_annual_pondmes,1)
+  
+  dataset$mrentabilidad_total = dataset$mcomisiones + dataset$mactivos_margen + dataset$mpasivos_margen
+  dataset$mrentabilidad_total_pondmes = dataset$mrentabilidad_total / dataset$cliente_antiguedad
+  dataset$mrentabilidad_sobre_total_pondmes = dataset$mrentabilidad / ifelse(dataset$mrentabilidad_total_pondmes>0, dataset$mrentabilidad_total_pondmes,1)
+  
+  #las sobre estan muy correlacionadas
+  
+  dataset$mcuenta_corriente_total = dataset$mcuenta_corriente + dataset$mcuenta_corriente_adicional
+  dataset$mcuenta_corriente_difadic = dataset$mcuenta_corriente_adicional - dataset$mcuenta_corriente #cuanto mas grande, no usa la premium, se va a ir
+  dataset$mcaja_ahorro_total = dataset$mcaja_ahorro + dataset$mcaja_ahorro_adicional + dataset$mcaja_ahorro_dolares
+  dataset$mcaja_ahorro_difadic = dataset$mcaja_ahorro_adicional - dataset$mcaja_ahorro #cuanto mas grande, no usa la premium, se va a ir
+  #total ya esta mcuentas_saldo
+  
+  dataset$mautoservicio_prom_transacciones = dataset$mautoservicio / ifelse(dataset$ctarjeta_debito_transacciones>0, dataset$ctarjeta_debito_transacciones,1)
+  dataset$mtarjeta_visa_consumo_prom_transacciones = dataset$mtarjeta_visa_consumo / ifelse(dataset$ctarjeta_visa_transacciones>0, dataset$ctarjeta_visa_transacciones,1)
+  dataset$mtarjeta_master_consumo_prom_transacciones = dataset$mtarjeta_master_consumo / ifelse(dataset$ctarjeta_master_transacciones>0, dataset$ctarjeta_master_transacciones,1)
+  
+  dataset$mprestamos_personales_prom = dataset$mprestamos_personales / ifelse(dataset$cprestamos_personales>0, dataset$cprestamos_personales,1)
+  dataset$mprestamos_prendarioss_prom = dataset$mprestamos_prendarios / ifelse(dataset$cprestamos_prendarios>0, dataset$cprestamos_personales,1)
+  dataset$mprestamos_hipotecarios_prom = dataset$mprestamos_hipotecarios / ifelse(dataset$cprestamos_hipotecarios>0, dataset$cprestamos_hipotecarios,1)
+  dataset$mprestamos_total=dataset$mprestamos_personales + dataset$mprestamos_prendarios + dataset$mprestamos_hipotecarios
+  dataset$mprestamos_total_prom = dataset$mprestamos_personales_prom + dataset$mprestamos_prendarioss_prom + dataset$mprestamos_hipotecarios_prom
+  
+  dataset$mplazo_fijo_total= dataset$mplazo_fijo_dolares + dataset$mplazo_fijo_pesos
+  dataset$mplazo_fijo_prom = dataset$mplazo_fijo_total / ifelse(dataset$cplazo_fijo>0, dataset$cplazo_fijo,1)
+  dataset$minversion1_total = dataset$minversion1_pesos + dataset$minversion1_dolares
+  dataset$minversion1_prom = dataset$minversion1_total / ifelse(dataset$cinversion1>0, dataset$cinversion1,1)
+  dataset$minversion2_prom =dataset$minversion2 / ifelse(dataset$cinversion2>0, dataset$cinversion2,1)
+  dataset$minversion_total = dataset$minversion1_total + dataset$minversion2
+  
+  dataset$mpayroll_prom = dataset$mpayroll / ifelse(dataset$cpayroll_trx>0, dataset$cpayroll_trx,1)
+  dataset$mpayroll2_prom = dataset$mpayroll2 / ifelse(dataset$cpayroll2_trx>0, dataset$cpayroll2_trx,1)
+  
+  dataset$mcuenta_debitos_automaticos_prom = dataset$mcuenta_debitos_automaticos / ifelse(dataset$ccuenta_debitos_automaticos>0, dataset$ccuenta_debitos_automaticos,1 )
+  dataset$mtarjeta_visa_debitos_automaticos_prom = dataset$mtarjeta_visa_debitos_automaticos / ifelse(dataset$ctarjeta_visa_debitos_automaticos>0, dataset$ctarjeta_visa_debitos_automaticos,1 )
+  dataset$mttarjeta_master_debitos_automaticos_prom = dataset$mttarjeta_master_debitos_automaticos / ifelse(dataset$ctarjeta_master_debitos_automaticos>0, dataset$ctarjeta_master_debitos_automaticos,1 )
+  
+  dataset$mpagodeservicios_prom = dataset$mpagodeservicios / ifelse(dataset$cpagodeservicios>0, dataset$cpagodeservicios,1 )
+  dataset$mpagomiscuentas_prom = dataset$mpagomiscuentas / ifelse(dataset$cpagomiscuentas>0, dataset$cpagomiscuentas,1 )
+  
+  dataset$mcajeros_propios_descuentos_prom = dataset$mcajeros_propios_descuentos / ifelse(dataset$ccajeros_propios_descuentos>0, dataset$ccajeros_propios_descuentos,1 )
+  dataset$mtarjeta_visa_descuentos_prom = dataset$mtarjeta_visa_descuentos / ifelse(dataset$ctarjeta_visa_descuentos>0, dataset$ctarjeta_visa_descuentos,1 )
+  dataset$mtarjeta_master_descuentos_prom = dataset$mtarjeta_master_descuentos / ifelse(dataset$ctarjeta_master_descuentos>0, dataset$ctarjeta_master_descuentos,1 )
+  
+  dataset$mcomisiones_mantenimiento_prom = dataset$mcomisiones_mantenimiento / ifelse(dataset$ccomisiones_mantenimiento>0, dataset$ccomisiones_mantenimiento,1 )
+  dataset$mcomisiones_otras_prom = dataset$mcomisiones_otras / ifelse(dataset$ccomisiones_otras>0, dataset$ccomisiones_otras,1 )
+  dataset$mcomisiones_difotras = dataset$mcomisiones_otras - dataset$mcomisiones_mantenimiento #cuanto mas grande, no usa la premium, se va a ir
+  dataset$mcomisiones_difotras_prom = dataset$mcomisiones_otras_prom - dataset$mcomisiones_mantenimiento_prom #cuanto mas grande, no usa la premium, se va a ir
+  
+  dataset$mforex_sell_prom = dataset$mforex_sell / ifelse(dataset$cforex_sell>0, dataset$cforex_sell,1 )
+  dataset$mforex_buy_prom = dataset$mforex_buy / ifelse(dataset$cforex_buy>0, dataset$cforex_buy,1 )
+
+  #valvula de seguridad para evitar valores infinitos
+  #paso los infinitos a NULOS
+  infinitos      <- lapply(names(dataset),function(.name) dataset[ , sum(is.infinite(get(.name)))])
+  infinitos_qty  <- sum( unlist( infinitos) )
+  if( infinitos_qty > 0 )
+  {
+    cat( "ATENCION, hay", infinitos_qty, "valores infinitos en tu dataset. Seran pasados a NA\n" )
+    dataset[mapply(is.infinite, dataset)] <<- NA
+  }
+
+
+  #valvula de seguridad para evitar valores NaN  que es 0/0
+  #paso los NaN a 0 , decision polemica si las hay
+  #se invita a asignar un valor razonable segun la semantica del campo creado
+  nans      <- lapply(names(dataset),function(.name) dataset[ , sum(is.nan(get(.name)))])
+  nans_qty  <- sum( unlist( nans) )
+  if( nans_qty > 0 )
+  {
+    cat( "ATENCION, hay", nans_qty, "valores NaN 0/0 en tu dataset. Seran pasados arbitrariamente a 0\n" )
+    cat( "Si no te gusta la decision, modifica a gusto el programa!\n\n")
+    dataset[mapply(is.nan, dataset)] <<- 0
+  }
+
+}
+#------------------------------------------------------------------------------
+#deflaciona por IPC
+#momento 1.0  31-dic-2020 a las 23:59
+
+drift_deflacion  <- function( campos_monetarios )
+{
+  vfoto_mes <- c( 201901, 201902, 201903, 201904, 201905, 201906,
+                  201907, 201908, 201909, 201910, 201911, 201912,
+                  202001, 202002, 202003, 202004, 202005, 202006,
+                  202007, 202008, 202009, 202010, 202011, 202012,
+                  202101, 202102, 202103, 202104, 202105, 202106,
+                  202107  )
+
+  vIPC  <- c( 1.9903030878, 1.9174403544, 1.8296186587,
+              1.7728862972, 1.7212488323, 1.6776304408,
+              1.6431248196, 1.5814483345, 1.4947526791,
+              1.4484037589, 1.3913580777, 1.3404220402,
+              1.3154288912, 1.2921698342, 1.2472681797,
+              1.2300475145, 1.2118694724, 1.1881073259,
+              1.1693969743, 1.1375456949, 1.1065619600,
+              1.0681100000, 1.0370000000, 1.0000000000,
+              0.9680542110, 0.9344152616, 0.8882274350,
+              0.8532444140, 0.8251880213, 0.8003763543,
+              0.7763107219  )
+
+  tb_IPC  <- data.table( "foto_mes"= vfoto_mes,
+                         "IPC" = vIPC )
+
+  dataset[ tb_IPC,
+           on= c("foto_mes"),
+           (campos_monetarios) :=  .SD * i.IPC ,
+           .SDcols = campos_monetarios ]
+
+}
+
+#------------------------------------------------------------------------------
+
+drift_rank_simple  <- function( campos_drift )
+{
+  for( campo in campos_drift )
+  {
+    cat( campo, " " )
+    dataset[ , paste0(campo,"_rank") :=  (frank( get(campo), ties.method="random") - 1) / ( .N -1 ), by= foto_mes]
+    dataset[ , (campo) := NULL ]
+  }
+}
+#------------------------------------------------------------------------------
+#El cero se transforma en cero
+#los positivos se rankean por su lado
+#los negativos se rankean por su lado
+
+drift_rank_cero_fijo  <- function( campos_drift )
+{
+  for( campo in campos_drift )
+  {
+    cat( campo, " " )
+    dataset[ get(campo) ==0, paste0(campo,"_rank") := 0 ]
+    dataset[ get(campo) > 0, paste0(campo,"_rank") :=   frank(  get(campo), ties.method="random")  / .N, by= foto_mes ]
+    dataset[ get(campo) < 0, paste0(campo,"_rank") :=  -frank( -get(campo), ties.method="random")  / .N, by= foto_mes ]
+    dataset[ , (campo) := NULL ]
+  }
+}
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Aqui comienza el programa
+
+setwd("~/buckets/b1")
+
+#cargo el dataset donde voy a entrenar
+#esta en la carpeta del exp_input y siempre se llama  dataset.csv.gz
+dataset_input  <- paste0( "./exp/", PARAM$exp_input, "/dataset.csv.gz" )
+dataset  <- fread( dataset_input )
+
+#creo la carpeta donde va el experimento
+dir.create( paste0( "./exp/", PARAM$experimento, "/"), showWarnings = FALSE )
+setwd(paste0( "./exp/", PARAM$experimento, "/"))   #Establezco el Working Directory DEL EXPERIMENTO
+
+
+
+#primero agrego las variables manuales
+AgregarVariables( dataset )
+
+#ordeno de esta forma por el ranking
+setorder( dataset, foto_mes, numero_de_cliente )
+
+#por como armé los nombres de campos, estos son los campos que expresan variables monetarias
+campos_monetarios  <- colnames(dataset)
+campos_monetarios  <- campos_monetarios[campos_monetarios %like% "^(m|Visa_m|Master_m|vm_m)"]
+
+#aqui aplico un metodo para atacar el data drifting
+#hay que probar experimentalmente cual funciona mejor
+switch( 
+PARAM$metodo,
+  "ninguno"        = cat( "No hay correccion del data drifting" ),
+  "rank_simple"    = drift_rank_simple( campos_monetarios ),
+  "rank_cero_fijo" = drift_rank_cero_fijo( campos_monetarios ),
+  "deflacion"      = drift_deflacion( campos_monetarios ) 
+)
+
+
+
+fwrite( dataset,
+        file="dataset.csv.gz",
+        sep= "," )
